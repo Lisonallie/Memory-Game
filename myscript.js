af91d3ee65ad7bac58beb57bd732911d7ef7119e
Lisonallie <allisonvanlinden@gmail.com>
2019-06-26 14:26:03 +0200

    get all cards to flip open

--- myscript.js
+++ myscript.js
@@ -2,12 +2,13 @@
 let card = document.getElementsByClassName("flip-card ");
 let innerCard = document.querySelectorAll(".flip-card-inner");
 let allCards = document.getElementById("firstdeck");
-let cardArray = []; 
+let cardArray = [];
+let innerArray = [];
 
-card[0].addEventListener("click", ()=>{
-    innerCard[0].style.cssText = "transform:rotateY(180deg);";
-});
 
 for (let i = 0; i < card.length; i++) {
-    cardArray.push(i);
+    card[i].addEventListener("click", () => {
+        innerCard[i].style.cssText = "transform:rotateY(180deg);";
+    });
+    cardArray.push();
 }
