81d03f00e44ed2300c46ae6640454ef001ec740a
Lisonallie <allisonvanlinden@gmail.com>
2019-06-26 16:58:08 +0200

    try to get function to work

--- myscript.js
+++ myscript.js
@@ -2,13 +2,17 @@
 let card = document.getElementsByClassName("flip-card ");
 let innerCard = document.querySelectorAll(".flip-card-inner");
 let allCards = document.getElementById("firstdeck");
-let cardArray = [];
-let innerArray = [];
+// let cardArray = [];
+
+card.forEach
 
 
-for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener("click", () => {
-        innerCard[i].style.cssText = "transform:rotateY(180deg);";
-    });
-    cardArray.push();
-}
+
+// for (let i = 0; i < card.length; i++) {
+//     card[i].addEventListener("click", () => {
+//         innerCard[i].style.cssText = "transform:rotateY(180deg);";
+//         clicked = 0;
+//     });
+//     cardArray.push();
+// }
+
