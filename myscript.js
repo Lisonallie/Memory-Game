7292ede5b67a4a7f05d4527bdd78c0b6aa1c0583
Lisonallie <allisonvanlinden@gmail.com>
2019-06-27 16:18:38 +0200

    add 2 cards

--- myscript.js
+++ myscript.js
@@ -1,10 +1,26 @@
 
 let innerCard = document.querySelectorAll(".inner-card");
 let cardArray = [];
+let cardImg = document.querySelectorAll(".flip-card-back > img");
+let firstCardClicked;
+let secondCardClicked;
+let firstFlip = true;
 
 for (let i = 0; i < innerCard.length; i++) {
-innerCard[i].addEventListener("click", function() {
-    innerCard[i].classList.toggle('is-flipped');
-  });
-  cardArray.push(innerCard[i]);
+    innerCard[i].addEventListener("click", function flip() {
+        innerCard[i].classList.toggle('is-flipped');
+        if (firstFlip) {
+            firstCardClicked = innerCard[i];
+            firstFlip = false;
+        }
+        else {
+            secondCardClicked = innerCard[i];
+            compare();
+        }
+    });
+    cardArray.push(innerCard[i]);
+}
+
+function compare() {
+
 }
