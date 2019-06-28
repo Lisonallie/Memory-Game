702e12e1c881d2efbfc27986c02c386ad7f40378
Lisonallie <allisonvanlinden@gmail.com>
2019-06-28 09:34:39 +0200

    start setting up recognition of clicks

--- myscript.js
+++ myscript.js
@@ -1,37 +1,64 @@
 
-let innerCard = document.querySelectorAll(".inner-card");
-let cardArray = [];
-let cardImg = document.querySelectorAll(".flip-card-back > img");
-let firstCardClicked;
-let secondCardClicked;
-let firstFlip = true;
+const cards = document.querySelectorAll(".inner-card");
+let hasFlippedCard = false;
 
-for (let i = 0; i < innerCard.length; i++) {
-    innerCard[i].addEventListener("click", function flip() {
-        innerCard[i].classList.toggle('is-flipped');
-        if (firstFlip) {
-            firstCardClicked = innerCard[i];
-            firstCardClicked.removeEventListener("click", flip);
-            firstFlip = false;
-        }
-        else {
-            secondCardClicked = innerCard[i];
-            secondCardClicked.removeEventListener("click", flip);
-            reset();
-        }
-    });
-    cardArray.push(innerCard[i]);
-}
+let firstCard = secondCard;
 
-function reset() {
-    firstCardClicked.classList.remove('is-flipped');
-    secondCardClicked.classList.remove('is-flipped');
-    firstCardClicked.addEventListener("click", flip);
-    secondCardClicked.addEventListener("click", flip);
-    firstCardClicked = null;
-    secondCardClicked = null;
-}
-// let flip = flip();
+
+function flipCard() {
+    this.classList.add('is-flipped');
+    if(hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+    }
+  }
+  
+  cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+
+
+
+
+
+
+// let cardArray = [];
+// let cardImg = document.querySelectorAll(".flip-card-back > img");
+
+// for (let i = 0; i < innerCard.length; i++) {
+//     innerCard[i].addEventListener("click", function flip() {
+//         innerCard[i].classList.toggle('is-flipped');
+//     });
+//     cardArray.push(innerCard[i]);
+// }
+
+
+
+// My code that never worked out
+// let firstCardClicked;
+// let secondCardClicked;
+// let firstFlip = true;
+    // function reset() {
+    //     firstCardClicked.classList.remove('is-flipped');
+    //     secondCardClicked.classList.remove('is-flipped');
+    //     firstCardClicked.addEventListener("click", flip);
+    //     secondCardClicked.addEventListener("click", flip);
+    //     firstCardClicked = null;
+    //     secondCardClicked = null;
+    // }
+    //     if (firstFlip) {
+    //         firstCardClicked = innerCard[i];
+    //         firstCardClicked.removeEventListener("click", flip);
+    //         firstFlip = false;
+    //     }
+    //     else {
+    //         secondCardClicked = innerCard[i];
+    //         secondCardClicked.removeEventListener("click", flip);
+    //         reset();
+    //     }
+    // });
+    // let flip = flip();
 
 // function compare() {
 //     if (firstCardClicked === secondCardClicked) {
